07d8a14185d5259c3c073af46770b0337f8f3071
NoNameGMM <gmm0119@163.com>
2025-02-05 21:34:11 +0800

    Fix Player

--- src/main/java/me/nonamegmm/mcscore/MCSCore.java
+++ src/main/java/me/nonamegmm/mcscore/MCSCore.java
@@ -5,8 +5,11 @@ import me.nonamegmm.mcscore.utils.Log;
 import me.nonamegmm.mcscore.utils.room.CreateRoom;
 import me.nonamegmm.mcscore.utils.room.JoinRoom;
 import me.nonamegmm.mcscore.utils.Init;
+import net.md_5.bungee.api.ChatMessageType;
+import net.md_5.bungee.api.chat.TextComponent;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -41,7 +44,7 @@ public final class MCSCore extends JavaPlugin implements Listener {
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         if(Objects.equals(args[0], "competitive")) {
             if(Objects.equals(args[1], "join")) {
-                joinRoom.joinCompetitive(sender.getName());
+                joinRoom.joinCompetitive((Player)sender);
             }
         }
         return false;
--- src/main/java/me/nonamegmm/mcscore/utils/room/JoinRoom.java
+++ src/main/java/me/nonamegmm/mcscore/utils/room/JoinRoom.java
@@ -1,14 +1,15 @@
 package me.nonamegmm.mcscore.utils.room;
 
 import me.nonamegmm.mcscore.utils.Log;
+import org.bukkit.entity.Player;
 
 import static me.nonamegmm.mcscore.utils.database.Database.checkTable;
 import static me.nonamegmm.mcscore.utils.database.Database.join;
 
 public class JoinRoom {
-    public void joinCompetitive(String name) {
+    public void joinCompetitive(Player player) {
         checkTable();
-        join(name);
+        join(player.getName());
         Log.info("正在尝试加入匹配");
     }
 }
