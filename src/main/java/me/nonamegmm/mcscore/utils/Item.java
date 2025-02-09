28b2454c95a94774f7492b2d6711bc60005b5301
NoNameGMM <gmm0119@163.com>
2025-02-09 21:55:36 +0800

    Use ItemStack and NBT-API instead of "/give"

--- src/main/java/me/nonamegmm/mcscore/utils/Item.java
+++ src/main/java/me/nonamegmm/mcscore/utils/Item.java
@@ -1,5 +1,19 @@
 package me.nonamegmm.mcscore.utils;
 
-public class Item {
+import de.tr7zw.changeme.nbtapi.NBTItem;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.Material;
 
+public class Item {
+    public static void getItem(Player player) {
+        ItemStack item = new ItemStack(Material.valueOf("TACZ_MODERN_KINETIC_GUN"));
+        NBTItem nbtItem = new NBTItem(item);
+        nbtItem.setString("GunId", "mcs2:cs_glock");
+        nbtItem.setString("GunFireMode", "SEMI");
+        nbtItem.setInteger("GunCurrentAmmoCount", 20);
+
+        // 给予玩家物品
+        player.getInventory().addItem(item);
+    }
 }
--- src/main/java/me/nonamegmm/mcscore/utils/room/JoinRoom.java
+++ src/main/java/me/nonamegmm/mcscore/utils/room/JoinRoom.java
@@ -1,11 +1,10 @@
 package me.nonamegmm.mcscore.utils.room;
 
+import me.nonamegmm.mcscore.utils.Item;
 import me.nonamegmm.mcscore.utils.Log;
 import me.nonamegmm.mcscore.utils.Message;
-import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.World;
-import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
 import static me.nonamegmm.mcscore.utils.database.Database.*;
@@ -21,11 +20,12 @@ public class JoinRoom {
             Location location = new Location(world, 5, 19, 36); // 设置一个默认的传送位置
             player.teleport(location);
             player.sendMessage("你已经被传送到世界: " + room);
-            CommandSender console = Bukkit.getConsoleSender();
-            Bukkit.dispatchCommand(console, "give " + player.getName() + " tacz:modern_kinetic_gun{GunId:\"mcs2:cs_glock\",GunFireMode:\"SEMI\",GunCurrentAmmoCount:20}");
-            Bukkit.dispatchCommand(console, "give " + player.getName() + " tacz:ammo{AmmoId:\"tacz:9mm\"} 60");
-            Bukkit.dispatchCommand(console, "give " + player.getName() + " tacz:ammo{AmmoId:\"tacz:9mm\"} 60");
-            Bukkit.dispatchCommand(console, "give " + player.getName() + " tacz:ammo{AmmoId:\"tacz:9mm\"} 60");
+            Item.getItem(player);
+            //CommandSender console = Bukkit.getConsoleSender();
+            //Bukkit.dispatchCommand(console, "give " + player.getName() + " tacz:modern_kinetic_gun{GunId:\"mcs2:cs_glock\",GunFireMode:\"SEMI\",GunCurrentAmmoCount:20}");
+            //Bukkit.dispatchCommand(console, "give " + player.getName() + " tacz:ammo{AmmoId:\"tacz:9mm\"} 60");
+            //Bukkit.dispatchCommand(console, "give " + player.getName() + " tacz:ammo{AmmoId:\"tacz:9mm\"} 60");
+            //Bukkit.dispatchCommand(console, "give " + player.getName() + " tacz:ammo{AmmoId:\"tacz:9mm\"} 60");
         }
         else {
             Message.sendActionBar(player, "你已经在房间里了");
