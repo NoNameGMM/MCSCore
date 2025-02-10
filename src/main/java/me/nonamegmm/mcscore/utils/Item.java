a6b0dea0b529b589421a37a042695c6a82b03242
NoNameGMM <gmm0119@163.com>
2025-02-10 17:47:04 +0800

    Fix unsupported function

--- src/main/java/me/nonamegmm/mcscore/utils/Item.java
+++ src/main/java/me/nonamegmm/mcscore/utils/Item.java
@@ -1,6 +1,6 @@
 package me.nonamegmm.mcscore.utils;
 
-import de.tr7zw.changeme.nbtapi.NBTItem;
+import de.tr7zw.changeme.nbtapi.NBT;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.Material;
@@ -8,10 +8,12 @@ import org.bukkit.Material;
 public class Item {
     public static void getItem(Player player) {
         ItemStack item = new ItemStack(Material.valueOf("TACZ_MODERN_KINETIC_GUN"));
-        NBTItem nbtItem = new NBTItem(item);
-        nbtItem.setString("GunId", "mcs2:cs_glock");
-        nbtItem.setString("GunFireMode", "SEMI");
-        nbtItem.setInteger("GunCurrentAmmoCount", 20);
+
+        NBT.modify(item, nbt -> {
+            nbt.setString("GunId", "mcs2:cs_glock");
+            nbt.setString("GunFireMode", "SEMI");
+            nbt.setInteger("GunCurrentAmmoCount", 20);
+        });
 
         // 给予玩家物品
         player.getInventory().addItem(item);
