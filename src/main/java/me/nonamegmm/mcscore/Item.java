9b8936d7d585fcbbb53be247ef1b930e24d13273
Hankary <johnneyzou@163.com>
2025-02-11 18:23:39 +0800

    Add new guns

--- src/main/java/me/nonamegmm/mcscore/Item.java
+++ src/main/java/me/nonamegmm/mcscore/Item.java
@@ -16,6 +16,7 @@ public class Item {
         });
         player.getInventory().addItem(item);
     }
+
     public static void get9mm(Player player) {
         ItemStack item = new ItemStack(Material.valueOf("TACZ_AMMO"), 60);
         NBT.modify(item, nbt -> {
@@ -23,4 +24,72 @@ public class Item {
         });
         player.getInventory().addItem(item);
     }
+
+    public static void getM4A1S(Player player) {
+        ItemStack item = new ItemStack(Material.valueOf("TACZ_MODERN_KINETIC_GUN"));
+        NBT.modify(item, nbt -> {
+            nbt.setString("GunId", "mcs2:cs_m4a1s");
+            nbt.setString("GunFireMode", "AUTO");
+            nbt.setInteger("GunCurrentAmmoCount", 20);
+            nbt.setByte("HasBulletInBarrel", (byte) 1);
+        });
+        player.getInventory().addItem(item);
+    }
+
+    public static void getAWP(Player player) {
+        ItemStack item = new ItemStack(Material.valueOf("TACZ_MODERN_KINETIC_GUN"));
+        NBT.modify(item, nbt -> {
+            nbt.setString("GunId", "mcs2:cs_awp");
+            nbt.setString("GunFireMode", "SEMI");
+            nbt.setInteger("GunCurrentAmmoCount", 5);
+            nbt.setByte("HasBulletInBarrel", (byte) 1);
+        });
+        player.getInventory().addItem(item);
+    }
+
+    public static void getAK47(Player player) {
+        ItemStack item = new ItemStack(Material.valueOf("TACZ_MODERN_KINETIC_GUN"));
+        NBT.modify(item, nbt -> {
+            nbt.setString("GunId", "mcs2:cs_ak");
+            nbt.setString("GunFireMode", "AUTO");
+            nbt.setInteger("GunCurrentAmmoCount",30);
+            nbt.setByte("HasBulletInBarrel", (byte) 1);
+        });
+        player.getInventory().addItem(item);
+    }
+
+    public static void getUSP(Player player) {
+        ItemStack item = new ItemStack(Material.valueOf("TACZ_MODERN_KINETIC_GUN"));
+        NBT.modify(item, nbt -> {
+            nbt.setString("GunId", "mcs2:cs_usp");
+            nbt.setString("GunFireMode", "SEMI");
+            nbt.setInteger("GunCurrentAmmoCount", 12);
+            nbt.setByte("HasBulletInBarrel", (byte) 1);
+        });
+        player.getInventory().addItem(item);
+    }
+
+    public static void getT_knife(Player player) {
+        ItemStack item = new ItemStack(Material.valueOf("TACZ_MODERN_KINETIC_GUN"));
+        NBT.modify(item, nbt -> {
+            nbt.setString("GunId", "mcs2:cs_t_knife");
+            nbt.setString("GunFireMode", "SEMI");
+            nbt.setInteger("GunCurrentAmmoCount", 1);
+            nbt.setByte("HasBulletInBarrel", (byte) 1);
+        });
+        player.getInventory().addItem(item);
+    }
+
+    public static void getCT_knife(Player player) {
+        ItemStack item = new ItemStack(Material.valueOf("TACZ_MODERN_KINETIC_GUN"));
+        NBT.modify(item, nbt -> {
+            nbt.setString("GunId", "mcs2:cs_ct_knife");
+            nbt.setString("GunFireMode", "SEMI");
+            nbt.setInteger("GunCurrentAmmoCount", 1);
+            nbt.setByte("HasBulletInBarrel", (byte) 1);
+        });
+        player.getInventory().addItem(item);
+    }
+
+
 }
